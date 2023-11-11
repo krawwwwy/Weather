87a6a88ca3bde2b8d627388afa134dd2fdcc2c63
Alexey Karcev <krakenlex@mail.ru>
2023-11-11 03:09:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,34 +9,34 @@ TOKEN: Final = '6552948051:AAF4dMTVjQU-fnqn8JhIMBW3Fe1mcU_OHnk'
 OPENWEATHERMAP_API_KEY = 'a5da20252a648f28a4a091f020b22d19'
 BOT_USERNAME: Final = '@Weather_Volgograd_bot'
 
-# Dictionary to store user-selected cities
 user_cities = {}
 
+
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет, это мой первый телеграм бот написанный при помощи python-telegram-bot. Нажми /help чтобы увидеть список команд")
+    await update.message.reply_text("Hello, this is my first Telegram bot, written using python-telegram-bot. Press /help to see a list of commands")
+
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('''Команда /weather показывает погоду в данный момент времени,
-    Команда /forecast - показывает прогноз на завтрашний день
-    Команда /weeklyforecast - показывает прогноз на неделю
-    Команда /setcity - установить город для прогноза погоды''')
+    await update.message.reply_text('''The /weather command shows the weather at a given time,
+Command /forecast - shows the forecast for tomorrow
+Command /weeklyforecast - shows the forecast for the week
+Command /setcity - set the city for the weather forecast''')
 
 async def set_city(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if context.args:
         user_id = update.message.from_user.id
-        # Combine all arguments as the city name
         city_name = ' '.join(context.args)
         user_cities[user_id] = city_name
-        await update.message.reply_text(f'Город установлен на {city_name}')
+        await update.message.reply_text(f'City set to {city_name}')
     else:
-        await update.message.reply_text('Используйте команду так: /setcity <город>')
+        await update.message.reply_text('Use the command like this: /setcity <сity>')
+
 
 async def weekly_forecast_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.from_user.id
 
-    # Prompt the user to set the city if not already set
     if user_id not in user_cities:
-        await update.message.reply_text('Пожалуйста, установите город с помощью команды /setcity <город>')
+        await update.message.reply_text('Please set the city using the command /setcity <city>')
         return
 
     city = quote(user_cities[user_id])
@@ -44,9 +44,8 @@ async def weekly_forecast_command(update: Update, context: ContextTypes.DEFAULT_
     api_url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={OPENWEATHERMAP_API_KEY}&units=metric'
     response = requests.get(api_url)
 
-    # Check if the API request was successful
     if response.status_code != 200:
-        await update.message.reply_text('Извините, произошла ошибка при получении погоды. Пожалуйста, убедитесь, что введенный город корректен.')
+        await update.message.reply_text('Sorry, there was an error getting the weather. Please make sure the city entered is correct.')
         return
 
     weather_data = response.json()
@@ -65,22 +64,21 @@ async def weekly_forecast_command(update: Update, context: ContextTypes.DEFAULT_
         daily_forecast[day]['temperatures'].append(temperature)
         daily_forecast[day]['descriptions'].append(description)
 
-    message_text = f'Недельный прогноз погоды в {user_cities[user_id]}:\n'
+    message_text = f'Weekly weather forecast for {user_cities[user_id]}:\n\n'
 
     for day, data in daily_forecast.items():
         avg_temperature = sum(data['temperatures']) / len(data['temperatures'])
         avg_description = max(set(data['descriptions']), key=data['descriptions'].count)  # Most frequent description
 
-        message_text += f'{day}: Средняя температура: {avg_temperature:.1f}°C, Общее описание: {avg_description}\n'
+        message_text += f'{day}: Average temperature: {avg_temperature:.1f}°C \n General description: {avg_description}\n\n'
 
     await update.message.reply_text(message_text)
 
 async def forecast_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.from_user.id
 
-    # Prompt the user to set the city if not already set
     if user_id not in user_cities:
-        await update.message.reply_text('Пожалуйста, установите город с помощью команды /setcity <город>')
+        await update.message.reply_text('Please set the city using the command /setcity <city>')
         return
 
     city = quote(user_cities[user_id])
@@ -91,9 +89,8 @@ async def forecast_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     api_url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={OPENWEATHERMAP_API_KEY}&units=metric'
     response = requests.get(api_url)
 
-    # Check if the API request was successful
     if response.status_code != 200:
-        await update.message.reply_text('Извините, произошла ошибка при получении прогноза. Пожалуйста, убедитесь, что введенный город корректен.')
+        await update.message.reply_text('Sorry, there was an error retrieving the forecast. Please make sure the city entered is correct.')
         return
 
     weather_data = response.json()
@@ -103,17 +100,16 @@ async def forecast_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if forecast:
         temperature = forecast[0]['main']['temp']
         description = forecast[0]['weather'][0]['description']
-        message_text = f'Прогноз погоды в {user_cities[user_id]} на завтра:\nТемпература: {temperature}°C\nОписание: {description}'
+        message_text = f'Weather forecast for {user_cities[user_id]} for tomorrow:\n\nTemperature: {temperature}°C\nDescription: {description}'
         await update.message.reply_text(message_text)
     else:
-        await update.message.reply_text("Не удалось получить прогноз на завтра")
+        await update.message.reply_text("Failed to get forecast for tomorrow")
 
 async def weather_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.from_user.id
 
-    # Prompt the user to set the city if not already set
     if user_id not in user_cities:
-        await update.message.reply_text('Пожалуйста, установите город с помощью команды /setcity <город на английском>')
+        await update.message.reply_text('Please set the city using the command /setcity <city>')
         return
 
     city = quote(user_cities[user_id])
@@ -121,9 +117,8 @@ async def weather_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHERMAP_API_KEY}&units=metric'
     response = requests.get(api_url)
 
-    # Check if the API request was successful
     if response.status_code != 200:
-        await update.message.reply_text('Извините, произошла ошибка при получении погоды. Пожалуйста, убедитесь, что введенный город корректен.')
+        await update.message.reply_text('Sorry, there was an error getting the weather. Please make sure the city entered is correct.')
         return
 
     weather_data = response.json()
@@ -131,7 +126,7 @@ async def weather_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     temperature = weather_data['main']['temp']
     description = weather_data['weather'][0]['description']
 
-    message_text = f'Текущая погода в {user_cities[user_id]}:\nТемпература: {temperature}°C\nОписание: {description}'
+    message_text = f'Current weather in {user_cities[user_id]}:\n\nTemperature: {temperature}°C\nDescription: {description}'
     await update.message.reply_text(message_text)
 
 async def error_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
